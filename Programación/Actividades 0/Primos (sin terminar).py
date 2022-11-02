07aea705d4d0ecc03e9b0524983bd6132efb1010
Angel <a.arrieta03soto@gmail.com>
2022-11-02 08:32:12 +0100

    Decimonoveno commit

--- Programación/Actividades 0/Primos (sin terminar).py
+++ Programación/Actividades 0/Primos (sin terminar).py
@@ -2,12 +2,14 @@ print("Escribe un número para saber si es primo o no")
 x = int(input("Introduce un número\t"))
 cont = int
 if x == 1:
-    print(f"{x} es primo")
+    print(f"{x} no es primo")
 else:
     cont = x - 1
-
-    while x % cont == 1 and cont > 1:
-        print(f"{x} no es primo")
-        cont = cont - 1
-    else:
-        print(f"{x} es primo")
+    while cont > 1:
+        res = x % cont
+        if res > 0 and res != 0:
+            cont -= 1
+        elif res == 0:
+            print(f"{x} no es primo")
+            exit()
+    print(f"{x} es primo")
