f91a42ce507677e3866707980810103d88cddf71
Angel <a.arrieta03soto@gmail.com>
2022-11-30 11:51:47 +0100

    Changes (52) commit

--- SSII/Jose/HashMashFichero.py
+++ SSII/Jose/HashMashFichero.py
@@ -1,46 +1,25 @@
-import threading
 import time
-
-inicio = time.time()
+import hashlib
 
 
 def searcher():
-    import hashlib
     crypted = "3a5b8990e60ffa74e15461d7547e0f62aa94d19de4e3de687bd31176f8b6d17b"
-    with open("passwords.txt", "r") as arch:
-        readable = arch.read()
-        conj = list(readable)
-        file = ['']
-
-        match = "".join(file)
-        matchbin = match.encode()
-        matchhit = hashlib.sha256(matchbin)
-        trycript = matchhit.hexdigest()
-
-        while trycript != crypted:
-            for cont in conj:
-                file[0] = cont
-
-                match = "".join(file)
-                matchbin = match.encode()
-                matchhit = hashlib.sha256(matchbin)
-                trycript = matchhit.hexdigest()
-
-                if trycript == crypted:
-                    arch.close()
-                    print(f"La contraseña es {file}")
-
-                else:
-                    continue
+    with open("passwords.txt", "r", errors='ignore') as arch:
+        chunk = arch.read()
+        passwords = chunk.split()
+        for match in passwords:
+            matchbin = match.encode()
+            matchhit = hashlib.sha256(matchbin)
+            trycript = matchhit.hexdigest()
+            if trycript != crypted:
+                continue
+            if trycript == crypted:
+                arch.close()
+                return match
 
 
-fin = time.time()
-print(f"ha tardado {round(fin - inicio, 3)} segundos en encontrarse")
-
-
-Hilo1 = threading.Thread(target=searcher)
-Hilo2 = threading.Thread(target=searcher)
-Hilo3 = threading.Thread(target=searcher)
-Hilo1.start()
-Hilo2.start()
-Hilo3.start()
+if __name__ == "__main__":
+    inicio = time.time()
+    print(f"La contraseña es {searcher()}")
+    fin = time.time()
+    print(f"ha tardado {round(fin - inicio, 3)} segundos en encontrarse")
--- SSII/Jose/Tarea contraseña_python.pdf/HashFB [5 Carácteres].py
+++ SSII/Jose/Tarea contraseña_python.pdf/HashFB [5 Carácteres].py
@@ -1,54 +1,41 @@
 import time
 import threading
-inicio = time.time()
+import hashlib
 
 
 def searcher():
-    import hashlib
     conthashed = "80b164a062d120cf8544b4f8efbdcd4d2b0f1e89a5238a5a24f7b93918230378"
     incognitas = list('ñlkjhgfdsapoiuytrewqmnbvcxzQWERTYUIOPASDFGHJKLÑZXCVBNM1234567890+-*')
     Force = ['', '', '', '', '']
-
-    force = "".join(Force)
-    forcebin = force.encode()
-    hashmash = hashlib.sha256(forcebin)
-    comp = hashmash.hexdigest()
-    print(f"[{force}]")
-
-    if comp == conthashed:
-        fin1 = time.time()
-        print(f"La contraseña es {force} y ha tardado {round(fin1-inicio,3)} segundos en ejecutarse")
-        exit()
-
-    while True:
+    for let in incognitas:
+        Force[0] = let
         for let in incognitas:
-            Force[0] = let
+            Force[1] = let
             for let in incognitas:
-                Force[1] = let
+                Force[2] = let
                 for let in incognitas:
-                    Force[2] = let
+                    Force[3] = let
                     for let in incognitas:
-                        Force[3] = let
-                        for let in incognitas:
-                            Force[4] = let
+                        Force[4] = let
 
-                            force = "".join(Force)
-                            forcebin = force.encode()
-                            hashmash = hashlib.sha256(forcebin)
-                            comp = hashmash.hexdigest()
-                            print(f"[{force}]")
-                            if comp == conthashed:
-                                fin2 = time.time()
-                                print(f"La contraseña es {force} y ha tardado {round(fin2-inicio,3)}segundos en ejecutarse")
-                                exit()
-                            else:
-                                continue
-    fin3 = time.time()
-    print(f"Contraseña no encontrada, {round(fin3-inicio,3)} segundos malgastados")
+                        force = "".join(Force)
+                        forcebin = force.encode()
+                        hashmash = hashlib.sha256(forcebin)
+                        comp = hashmash.hexdigest()
+                        if comp == conthashed:
+                            succes = f"La contraseña es {force}"
+                            return print(succes)
+                        else:
+                            continue
 
-Hilo1 = threading.Thread(target=searcher)
-Hilo2 = threading.Thread(target=searcher)
-Hilo3 = threading.Thread(target=searcher)
-Hilo1.start()
-Hilo2.start()
-Hilo3.start()
+
+if __name__ == "__main__":
+    inicio = time.time()
+    Hilo1 = threading.Thread(target=searcher())
+    Hilo2 = threading.Thread(target=searcher())
+    Hilo3 = threading.Thread(target=searcher())
+    Hilo1.start()
+    Hilo2.start()
+    Hilo3.start()
+    fin = time.time()
+    print(f"Ha tardado {round(fin - inicio, 3)} segundos")
