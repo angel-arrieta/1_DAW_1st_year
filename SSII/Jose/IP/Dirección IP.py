90b0e1c22af90dcef66aa8d44952499e3ba235fa
Angel <a.arrieta03soto@gmail.com>
2022-12-01 12:15:46 +0100

    Change (56) commit

--- SSII/Jose/IP/Dirección IP.py
+++ SSII/Jose/IP/Dirección IP.py
@@ -1,39 +1,66 @@
+def ip_checker(value):
+    octetos = value.split(".", 3)
+    octetos[0] = int(octetos[0])
+    octetos[1] = int(octetos[1])
+
+    if 0 <= octetos[0] <= 127:
+        tipo = "A (red grande)"
+        if octetos[0] == 10:
+            reach = "[privada]"
+        else:
+            reach = "[pública]"
+
+    elif 128 <= octetos[0] <= 191:
+        tipo = "B (red mediana)"
+        if octetos[0] == 172 and 16 <= octetos[1] <= 31:
+            reach = "[privada]"
+        else:
+            reach = "[pública]"
+
+    elif 192 <= octetos[0] <= 223:
+        tipo = "C (red pequeña)"
+        if octetos[0] == 192 and octetos[1] == 168:
+            reach = "[privada]"
+        else:
+            reach = "[pública]"
+    check = f"IP tipo {tipo} {reach}"
+    return check
+
+
 def validator(value):
     parts = value.split(".", 3)
     for part in parts:
-        if int(part) > 255:
+        part = int(part)
+
+        if not 0 <= part <= 255:
             invalid = "La IP no es válida"
             return print(invalid), exit()
         else:
             return
 
 
-def privacy(value):
-    if int(value) >= 224:
-        public = "La IP es pública"
-        return print(public)
-    else:
-        return
-
-
 def reserved(value):
+    value = int(value)
     if value == 0 or 255:
         reservada = "La IP es reservada [broadcast]"
-        return print(reservada)
+        return reservada
     else:
         return
 
 
-def IPtype(value):
-    value = 0
-    return value
+def d_check(value):
+    if int(value) >= 224:
+        return True
+    else:
+        return False
 
 
 if __name__ == "__main__":
     IP = input(">")
-    octetos = IP.split(".", 3)
-    # www.xxx.yyy.zzz   www=[0]   xxx=[1]   yyy=[2]   zzz=[3]
+    octeto = IP.split(".", 3)
     validator(IP)
-    privacy(octetos[0])
-    reserved(octetos[3])
-    IPtype()
+    print(reserved(octeto[3]))
+    if d_check(octeto[0]) is True:
+        print("IP tipo D [pública]")
+    else:
+        print(ip_checker(IP))
