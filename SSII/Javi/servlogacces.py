dedc0467c87d94b59f4a67ab37acf6649a398932
Angel <a.arrieta03soto@gmail.com>
2023-05-23 10:08:10 +0200

    Change (92) commit

--- SSII/Javi/servlogacces.py
+++ SSII/Javi/servlogacces.py
@@ -3,15 +3,10 @@ from paramiko import SSHClient, AutoAddPolicy
 ssh = SSHClient()
 ssh.set_missing_host_key_policy(AutoAddPolicy())
 ssh.connect("172.26.1.194", username="usuario", password="usuario")
-
-
-remote_path = "/ruta/al/log.log"  # Reemplaza "/ruta/al/log.log" por la ruta correcta del log en el servidor
-
-
 sftp = ssh.open_sftp()
 
 if __name__ == "__main__":
-    with sftp.open(remote_path, "r") as log_file:
+    with sftp.open("/var/log/nginx/access.log", "r") as log_file:
         lines = log_file.readlines()[-10:]
 
     with open("index.html", "w") as index_file:
