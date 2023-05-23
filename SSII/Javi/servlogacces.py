2f69ecaf8d7e428c2a14f3f5b519e71727d7731a
Angel <a.arrieta03soto@gmail.com>
2023-05-23 10:21:46 +0200

    Change (93) commit

--- SSII/Javi/servlogacces.py
+++ SSII/Javi/servlogacces.py
@@ -1,4 +1,5 @@
 from paramiko import SSHClient, AutoAddPolicy
+import time
 
 ssh = SSHClient()
 ssh.set_missing_host_key_policy(AutoAddPolicy())
@@ -6,18 +7,21 @@ ssh.connect("172.26.1.194", username="usuario", password="usuario")
 sftp = ssh.open_sftp()
 
 if __name__ == "__main__":
-    with sftp.open("/var/log/nginx/access.log", "r") as log_file:
-        lines = log_file.readlines()[-10:]
+    while True:
+        with sftp.open("/var/log/nginx/access.log", "r") as log_file:
+            lines = log_file.readlines()[-10:]
 
-    with open("index.html", "w") as index_file:
-        index_file.write("<html>\n")
-        index_file.write("<body>\n")
-        index_file.write("<pre>\n")
-        index_file.writelines(lines)
-        index_file.write("</pre>\n")
-        index_file.write("</body>\n")
-        index_file.write("</html>\n")
+        with open("index.html", "w") as index_file:
+            index_file.write("<html>\n")
+            index_file.write("<body>\n")
+            index_file.write("<pre>\n")
+            index_file.writelines(lines)
+            index_file.write("</pre>\n")
+            index_file.write("</body>\n")
+            index_file.write("</html>\n")
 
-    print("Se ha creado el archivo index.html con las últimas líneas del log.")
+        print("Se ha creado el archivo index.html con las últimas líneas del log.")
+        time.sleep(3)
+
     sftp.close()
     ssh.close()
